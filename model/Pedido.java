1875c389480299a19cc8c19891ca30749ce41364
PedroGuilhermeYS <pedro210905@gmail.com>
2025-06-24 23:17:44 -0300

    Realizei a implementação das principais classes, o main/entrada e ações, Entrada.java excluída por não considerar útil

--- model/Pedido.java
+++ model/Pedido.java
@@ -1,9 +1,47 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
+    private Cliente cliente;
+    private List<ItemPedido> itens;
+    //private String tipoFrete; Necessita classes Service
+    private double valorFrete;
 
-	public Pedido() {
-		// TODO Auto-generated constructor stub
-	}
+    public Pedido(Cliente cliente, String tipoFrete) {
+        this.cliente = cliente;
+        //this.tipoFrete = tipoFrete; Necessita classes Service
+        this.itens = new ArrayList<>();
+    }
 
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void adicionarItem(Produto p, int qtd) {
+        this.itens.add(new ItemPedido(p, qtd));
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (ItemPedido item : itens) {
+            total += item.getSubtotal();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "cliente=" + cliente.getNome() +
+                ", total=" + calcularTotal() +
+                ", frete=" + valorFrete +
+                //", totalComFrete=" + TotalComFrete() + Necessita classes Service
+                '}';
+    }
 }
